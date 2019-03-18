93b337e88105e2c899776562e2047f3899cc53c1
Garynan520 <475923654@qq.com>
2019-03-18 11:16:47 +0800

    add index.html template to all demo, and init weboack-tree-shaking demo

--- webpack-assest-management/webpack.config.js
+++ webpack-assest-management/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -40,5 +42,15 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['dist/**'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'webpack-assest-management',
+      template: './index.html'
+    }),
+  ]
 };
--- webpack-tree-shaking/webpack.config.js
+++ webpack-tree-shaking/webpack.config.js
@@ -8,13 +8,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  /* 开发模式 */
+  mode: 'development',
+  /* 备注为使用的 export */
+  optimization: {
+    usedExports: true
+  },
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ['dist/**'],
     }),
     new HtmlWebpackPlugin({
-      title: 'webpack-basic-usage',
+      title: 'webpack-tree-shaking',
       template: './index.html'
     }),
   ]
